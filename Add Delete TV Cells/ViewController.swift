d784ac3061933a53a47de952ee3c013cb6a7caf6
Prashant G <prashant.gaikwad@leftrightmind.com>
2019-01-17 22:24:07 +0530

    Add Delete functionality

--- Add Delete TV Cells/ViewController.swift
+++ Add Delete TV Cells/ViewController.swift
@@ -9,12 +9,60 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var desserts = [String]()
+    
+    @IBAction func onAddTapped() {
+        let alert = UIAlertController(title: "Add Dessert", message: nil, preferredStyle: .alert)
+        alert.addTextField { (dessertTF) in
+            dessertTF.placeholder = "Enter Dessert"
+        }
+        let action = UIAlertAction(title: "Add", style: .default) { (_) in
+            guard let dessert = alert.textFields?.first?.text else { return }
+            if dessert != "" {
+                print(dessert)
+                self.add(dessert)
+            }
+        }
+        alert.addAction(action)
+        present(alert, animated: true)
     }
-
-
+    
+    func add(_ dessert: String) {
+        let index = 0
+        desserts.insert(dessert, at: index)
+        
+        let indexPath = IndexPath(row: index, section: 0)
+        tableView.insertRows(at: [indexPath], with: .left)
+    }
+    
 }
+
+extension ViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return desserts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let dessert = desserts[indexPath.row]
+        cell.textLabel?.text = dessert
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        guard editingStyle == .delete else { return }
+        desserts.remove(at: indexPath.row)
+        
+        tableView.deleteRows(at: [indexPath], with: .automatic)
+    }
+}
+
+
 
